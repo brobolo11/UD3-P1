b6a453eaa7ce32f9f4c3189771241d40f9c85120
brobolo11 <brofandino@gmail.com>
2025-03-19 23:47:13 +0100

    Falta keyframes

--- discos.js
+++ discos.js
@@ -1,12 +1,26 @@
-const audio = document.getElementById('myAudio');
-const btn = document.getElementById('audioBtn');
+document.addEventListener("DOMContentLoaded", function () {
+    const audios = document.getElementsByClassName("myAudio");
+    const buttons = document.getElementsByClassName("audioBtn");
 
-btn.addEventListener('click', function() {
-    if (audio.paused) {
-        audio.play();
-        btn.textContent = 'Pause';
-    } else {
-        audio.pause();
-        btn.textContent = 'Play';
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function () {
+            
+            // Pausar el audio que este sonando
+            for (let j = 0; j < audios.length; j++) {
+                if (j !== i) {
+                    audios[j].pause();
+                    audios[j].currentTime = 0; // Reiniciar audio
+                    buttons[j].textContent = "Play";
+                }
+            }
+
+            if (audios[i].paused) {
+                audios[i].play();
+                buttons[i].textContent = "Pause";
+            } else {
+                audios[i].pause();
+                buttons[i].textContent = "Play";
+            }
+        });
     }
 });
